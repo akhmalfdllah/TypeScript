932ef37295babe40c6997bd10e536e2f6cb45d74
Akhmal Faidillah <akmal.faidillah@gmail.com>
2024-12-03 10:43:20 +0700

    desk

--- src/model/user-model.ts
+++ src/model/user-model.ts
@@ -1,11 +1,20 @@
+import { User } from "@prisma/client"
+
 export type UserResponse = {
     username: string;
     name: string;
     token?: string;
 }
 
-export type UserRequest = {
+export type CreateUserRequest = {
     username: string;
     name: string;
     password: string;
+}
+
+export function toUserResponse(user: User): UserResponse {
+    return {
+        name: user.name,
+        username: user.username
+    }
 }
--- src/service/user-service.ts
+++ src/service/user-service.ts
@@ -1,11 +1,11 @@
 import { prismaClient } from "../application/database";
 import { ResponseError } from "../error/response-error";
-import { UserResponse } from "../model/user-model";
+import { CreateUserRequest, toUserResponse, UserResponse } from "../model/user-model";
 import { UserValidation } from "../validation/user-validation";
 import { Validation } from "../validation/validation";
 import bcrypt from "bcrypt";
 
-export class UserServie {
+export class UserService {
     static async register(request: CreateUserRequest): Promise<UserResponse> {
         const registerRequest = Validation.validate(UserValidation.REGISTER, request);
 
@@ -15,7 +15,7 @@ export class UserServie {
             }
         })
 
-        if (totalUserWithSameUsername != 0) {
+        if (totalUserWithSameUsername != 0 ) {
             throw new ResponseError(400, "Username already exists");
         }
 
@@ -24,5 +24,7 @@ export class UserServie {
         const user = await prismaClient.user.create({
             data: registerRequest
         });
+
+        return toUserResponse(user)
     }
 }
